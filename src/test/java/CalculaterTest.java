2c94b1a5694f89ded4623feedfa25d453a504c14
Nicklas Johansson <lurvas4ever@hotmail.com>
2019-11-19 13:54:07 +0100

    Added console outprint

--- src/test/java/CalculaterTest.java
+++ src/test/java/CalculaterTest.java
@@ -13,48 +13,54 @@ public class CalculaterTest {
     @Order(3)
     @DisplayName("\t➕\t\"Did i turn off the stov??\" \uD83D\uDE31")
     @CsvSource(value = {"1:1:2","2.5:2.5:5","4:2:6","257:352:609"}, delimiter = ':')
-    void calcAddition_ShouldReturnSumOFTheFirstTwoValueInSource(Double num1,Double num2,double num3){
+    void calcAddition_ShouldReturnSumOFTheFirstTwoValueInSource(Double num1,Double num2,double num3,TestInfo testInfo){
             assertEquals(num3, c.calcAddition(num1, num2));
             assertDoesNotThrow(()->c.calcAddition(num2,num1));
+        System.out.println(testInfo.getDisplayName());
     }
 
     @Test
     @Order(1)
     @DisplayName("\t\uD83D\uDD31\t\"Psycho\" by Hitchcock \n is a classic ")
-    void calcArraySize_ShouldReturnSizeOfAmoutOfNumbersInSource(){
+    void calcArraySize_ShouldReturnSizeOfAmoutOfNumbersInSource(TestInfo testInfo){
          int[] numbArray={2,4,6,7006,80,1,0,9};
         assertEquals(8,c.calcArraySize(numbArray));
         assertDoesNotThrow(()->c.calcArraySize(numbArray));
+        System.out.println(testInfo.getDisplayName());
     }
 
     @RepeatedTest(7)
     @Order(2)
     @DisplayName("➗ I can say Chimichanga in seven languages")
-    void calcDivide_shouldReturnSmallerDouble(){
+    void calcDivide_shouldReturnSmallerDouble(TestInfo testInfo){
 
         assertThrows(ArithmeticException.class, () -> {
             c.calcDivsion(1,0); });
         assertDoesNotThrow(()->c.calcDivsion(2,1));
+        System.out.println(testInfo.getDisplayName());
     }
     @Test
     @Order(4)
     @DisplayName("\t✖\t \"It's only a flesh wound.\" \n–Monty Python and the Holy Grail")
-    void calcMltiplication_ShouldReturnAMultipliedNumber(){
+    void calcMltiplication_ShouldReturnAMultipliedNumber(TestInfo testInfo){
         assertDoesNotThrow(() ->  c.calcMltiplication(0,0));
     assertEquals(100,c.calcMltiplication(10,10));
+        System.out.println(testInfo.getDisplayName());
     }
     @Test
     @Order(5)
     @DisplayName("\t➖ \t\"May de force be with you \"")
-    void calcSubstract_ShouldReturnALowerResult(){
+    void calcSubstract_ShouldReturnALowerResult(TestInfo testInfo){
         assertDoesNotThrow(() ->  c.calcSubstract(0,0));
         assertEquals(666,c.calcSubstract(999,333));
+        System.out.println(testInfo.getDisplayName());
     }
     @RepeatedTest(3)
     @Order(6)
     @DisplayName("If you feel lonly, see a horror movie \uD83D\uDE08")
-    void calcServalMethods(){
+    void calcServalMethods(TestInfo testInfo){
         assertDoesNotThrow(() -> assertEquals(1,c.calcSubstract(c.calcMltiplication((int)c.calcAddition(2,3),2),9)));
+        System.out.println(testInfo.getDisplayName());
     }
     @BeforeEach
      void washYourHandsBeforeDinner(){
